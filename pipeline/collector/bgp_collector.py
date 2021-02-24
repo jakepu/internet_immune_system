655817832e8f7c1a73e425d4b47e842ae8db84ca
Jake Pu <pujianhao@126.com>
2021-02-23 19:26:41 -0600

    bgp parser rough debug done

--- pipeline/collector/bgp_collector.py
+++ pipeline/collector/bgp_collector.py
@@ -2,7 +2,7 @@ import datetime
 import sys
 import os
 import subprocess
-from base_collector import BaseCollector
+from .base_collector import BaseCollector
 class BGPCollector(BaseCollector):
     def __init__(self, source_url = '', local_path = '', dry_run = None):
         if source_url == '':
@@ -20,10 +20,8 @@ class BGPCollector(BaseCollector):
             dry_run = ''
         else:
             dry_run = '-n'
-        if subprocess.check_output(["rsync","-aiz", dry_run, self.source_url, self.local_path], text=True) == '':
+        if subprocess.check_output(["rsync","-aiz", dry_run, self.source_url, self.local_path]) == '':
             print('BGP Collector found no new files')
             return False
         print('BGP Collector rsynced new files')
         return True
-if __name__ == '__main__':
-    BGPCollector(dry_run=True)
--- pipeline/parser/bgp_parser.py
+++ pipeline/parser/bgp_parser.py
@@ -5,18 +5,19 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
 import numpy as np
-from ..collector.bgp_collector import BGPCollector
-from ..data_structure.parsed_obj import ParsedObj
 from multiprocessing import Pool
-from .base_parser import BaseParser
 import csv
 from multiprocessing import Pool
+from collector.bgp_collector import BGPCollector
+from data_structure.parsed_obj import ParsedObj
+from parser.base_parser import BaseParser
+
 
 class BGPParser(BaseParser):
 
-    def __init__(self): # actual deployment: collector = BGPCollector()
+    def __init__(self, dry_run=None): # actual deployment: collector = BGPCollector()
         # self._data_src = collector.local_path
-        self.collector = BGPCollector()
+        self.collector = BGPCollector(dry_run=dry_run)
         self._data_src = self.collector.local_path
         self._dump_exec_path = os.path.dirname(os.path.realpath(__file__)) + '/../externals/bgpdump/bgpdump'
         self._dump_exec_path = os.path.realpath(self._dump_exec_path)
@@ -52,7 +53,7 @@ class BGPParser(BaseParser):
         else:
             # self._curr_update_reader is not None
             # snapshot exhausted, return next row in update
-            row = next(self._curr_update_reader, default=None)
+            row = next(self._curr_update_reader, None)
             if row is None:
                 # current update exhausted
                 self._curr_update_csv_f.close()
@@ -88,7 +89,7 @@ class BGPParser(BaseParser):
         if self._last_update_time is None:
             raise RuntimeError('BGP parser is not initialized yet')
         flag = False
-        dir_name = self._data_src + '/{}.{}/UPDATES/'.format(self._last_update_time.year, self._last_update_time.month)
+        dir_name = self._data_src + '/{}.{:02}/UPDATES/'.format(self._last_update_time.year, self._last_update_time.month)
         for update_filename in sorted(os.listdir(dir_name)): # update_filename : updates.20200901.0000.bz2
             if flag == True:
                 self._last_update_filename = update_filename
@@ -100,7 +101,7 @@ class BGPParser(BaseParser):
         if update_filename == self._last_update_filename:
             # meaning this update is the last update in this month
             tmp_time = self._last_update_time +timedelta(weeks = 4) # go to next month by adding 28 days (potential bug)
-            dir_name = self._data_src + '/{}.{}/UPDATES/'.format(tmp_time.year, tmp_time.month)
+            dir_name = self._data_src + '/{}.{:02}/UPDATES/'.format(tmp_time.year, tmp_time.month)
             if (os.path.isdir(dir_name)): # if next month's data is available
                 for update_filename in sorted(os.listdir(dir_name)): # update_filename : updates.20200901.0000.bz2
                     self._last_update_filename = update_filename
@@ -124,27 +125,25 @@ class BGPParser(BaseParser):
     def _read_rib_from_file(self, start_time):
         if start_time <= datetime(2003,2,3,19,32,tzinfo=timezone.utc):
             raise LookupError('Data before 2003/02/03 19:32 UTC is in Pacific Time, algorithm not implemented for tricky data')
-        dir_name = self._data_src + '/{}.{}/RIBS/'.format(start_time.year, start_time.month)
+        dir_name = self._data_src + '/{}.{:02}/RIBS/'.format(start_time.year, start_time.month)
         # the following two line work as an edge detector - detect the first update after start_time
-        last_state = False
-        state = False
         rib_filename_list = sorted(os.listdir(dir_name))
         rib_filename_list = tuple(filter(lambda rib_filename: rib_filename.endswith('.bz2'), rib_filename_list))
         if start_time < self._filename_to_dt(rib_filename_list[0]):
             # if oldest rib in this month is older than the start_time, use the newest rib file from last month
             one_day_before = start_time - timedelta(days=1)
-            dir_name = self._data_src + '/{}.{}/RIBS/'.format(one_day_before.year, one_day_before.month)
+            dir_name = self._data_src + '/{}.{:02}/RIBS/'.format(one_day_before.year, one_day_before.month)
             rib_filename_list = sorted(os.listdir(dir_name))
             rib_filename_list = tuple(filter(lambda rib_filename: rib_filename.endswith('.bz2'), rib_filename_list))
             rib_filename = rib_filename_list[-1]
             rib_timestamp = self._filename_to_dt(rib_filename)
-            update_dir_name = self._data_src + '/{}.{}/UPDATES/'.format(one_day_before.year, one_day_before.month)
+            update_dir_name = self._data_src + '/{}.{:02}/UPDATES/'.format(one_day_before.year, one_day_before.month)
         else:
             for rib_filename in rib_filename_list: # rib_filename : rib.20200901.0000.bz2
                 rib_timestamp = self._filename_to_dt(rib_filename)
                 if start_time - rib_timestamp < self._delta_cap and start_time - rib_timestamp >= timedelta(hours=0):
                     break
-            update_dir_name = self._data_src + '/{}.{}/UPDATES/'.format(start_time.year, start_time.month)
+            update_dir_name = self._data_src + '/{}.{:02}/UPDATES/'.format(start_time.year, start_time.month)
         rib_csv_path = self._dump_path+'/'+rib_filename+'.csv'
         f = open(rib_csv_path,'w')
         subprocess.check_call([self._dump_exec_path, '-m', '-u', dir_name+'/'+rib_filename], stdout=f)
@@ -157,19 +156,30 @@ class BGPParser(BaseParser):
                 rib_rows[key] = row
         for update_filename in sorted(os.listdir(update_dir_name)): # update_filename : updates.20200901.0000.bz2
             update_timestamp = self._filename_to_dt(update_filename)
-            if update_timestamp >= rib_timestamp and update_timestamp < start_time:
-                state = True
+            if update_timestamp >= rib_timestamp and update_timestamp <= start_time:
                 update_csv_path = self._dump_path+'/'+update_filename+'.csv'
-                rib_rows = self._update_rib(rib_rows, update_csv_path, update_dir_name, update_filename)
-            else:
-                state = False
-            if last_state == True and state == False:
-                subprocess.check_call(['rm', '-f', rib_csv_path])
-                rib_rows = list(sorted(rib_rows.values(), key=lambda row: row['unix time in seconds']), reverse=True)
-                return rib_rows, update_filename
-            last_state = state
+                rib_rows, update_timestamp = self._update_rib(rib_rows, update_csv_path, update_dir_name, update_filename)
+                if update_timestamp > start_time:
+                    break
+            elif update_timestamp > start_time:
+                break
+        if update_timestamp <= start_time:
+            tmp_time = update_timestamp +timedelta(weeks = 4) # go to next month by adding 28 days (potential bug)
+            update_dir_name = self._data_src + '/{}.{:02}/UPDATES/'.format(tmp_time.year, tmp_time.month)
+            if os.path.isdir(update_dir_name):
+                for update_filename in sorted(os.listdir(update_dir_name)): # update_filename : updates.20200901.0000.bz2
+                    update_timestamp = self._filename_to_dt(update_filename)
+                    if update_timestamp >= rib_timestamp and update_timestamp <= start_time:
+                        update_csv_path = self._dump_path+'/'+update_filename+'.csv'
+                        rib_rows, update_timestamp = self._update_rib(rib_rows, update_csv_path, update_dir_name, update_filename)
+                        if update_timestamp > start_time:
+                            break
+                    elif update_timestamp > start_time:
+                        break
+        rib_rows = list(sorted(rib_rows.values(), key=lambda row: row['unix time in seconds'], reverse=True))
         subprocess.check_call(['rm', '-f', rib_csv_path])
-        return None, ''
+        return rib_rows, update_filename
+
     def _filename_to_dt(self, filename):
         date_str = filename.split('.')[1] 
         hour_str = filename.split('.')[2]
@@ -181,13 +191,14 @@ class BGPParser(BaseParser):
         f.close()
         update_csv_f = open(update_csv_path, 'r', newline='')
         update_reader = csv.DictReader(update_csv_f, fieldnames=self._names, delimiter='|')
-        row = next(update_reader,default=None)
+        row = next(update_reader,None)
         while row is not None:
-            if datetime.fromtimestamp(float(row['unix time in seconds']), tz=timezone.utc) > self.start_time:
+            update_timestamp = datetime.fromtimestamp(float(row['unix time in seconds']), tz=timezone.utc)
+            if update_timestamp > self.start_time:
                 # there are unread rows in the update, save file handler and csv reader (iterator)
                 self._curr_update_csv_f = update_csv_f
                 self._curr_update_reader = update_reader
-                return rib_rows
+                return rib_rows, update_timestamp
             key = (row['PeerAS'], row['Prefix'])
             if row['Withdraw or Announce'] == 'W':
                 try:
@@ -196,12 +207,11 @@ class BGPParser(BaseParser):
                     pass
             else:
                 rib_rows[key] = row
-            row = next(update_reader,default=None)
+            row = next(update_reader,None)
         # all rows in this update are applied, remove this csv
         update_csv_f.close()
         os.remove(update_csv_f.name)
-        # subprocess.check_call(['rm', '-f', update_csv_path])
-        return rib_rows
+        self._curr_update_csv_f = None
+        self._curr_update_reader = None
+        return rib_rows, update_timestamp
 
-if __name__ == '__main__':
-    pass
