5d35854ac3345982b6a33f6e87e93b3a42755d9e
Jake Pu <pujianhao@126.com>
2021-03-09 17:29:38 -0600

    interenet untested

--- pipeline/data_structure/autonomous_system.py
+++ pipeline/data_structure/autonomous_system.py
@@ -3,9 +3,12 @@ import numpy as np
 # waiting for databases and parsers to add attributes
 # keep three sets of neighbors, Customer, Peer, Provider
 
+class ASs():
+    def __init__(self):
+        self.dict = {}
 class AS:
     geo_location_set = ['Asia', 'US', 'Europe', 'Australia', 'Africa','SouthernAmerica']
     as_type_set = ['enterprise', 'private', 'non-profit']
-    def __init__(self, as_type=None, geo_location=None, routers = None, number=None, customers=None, peers=None, providers=None, router_list=None):
-        self.router_list = router_list
-        return
+    def __init__(self, number, as_type=None, geo_location=None, routers = None, customers=None, peers=None, providers=None, router_set=None):
+        self.router_set = router_set if router_set else set()
+        self.number = number
